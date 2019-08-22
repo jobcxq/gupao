3581561dfd8e7f950787f64d52b2ed281a451c75
jobcxq <jobcxq@163.com>
2019-08-22 23:33:33 +0800

    'curator'

--- zookeeper/src/test/java/com/cxq/zookeeper/curator/ZKBaseOperation.java
+++ zookeeper/src/test/java/com/cxq/zookeeper/curator/ZKBaseOperation.java
@@ -1,6 +1,10 @@
 package com.cxq.zookeeper.curator;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
@@ -45,4 +49,34 @@ public class ZKBaseOperation {
         curatorFramework.delete().forPath("/data");
         System.out.println("delete success!");
     }
+
+
+
+    @Test
+    public void addListenerWithNode() throws Exception {
+        //配置中心：创建、修改、删除
+        NodeCache nodeCache = new NodeCache(curatorFramework,"/watch",false);
+        NodeCacheListener listener = () -> {
+            System.out.println("receive Node Changed");
+            System.out.println(nodeCache.getCurrentData().getPath() + " : "
+                    + new String(nodeCache.getCurrentData().getData()));
+        };
+//        nodeCache.start(true);    //NodeCache第一次启动就会立刻从zk读取节点数据并保存在Cache中
+        nodeCache.start();          //默认false
+    }
+
+    @Test
+    public void addListenerWithChildNode() throws Exception {
+        //服务注册中心：可以针对服务做动态感知
+        PathChildrenCache pathChildCache = new PathChildrenCache(curatorFramework,"/watch/children",false);
+        PathChildrenCacheListener listener = (framework, event) -> {
+            System.out.println("receive Node Changed");
+            System.out.println(event.getType() + " : "  //CHILD_ADDED、CHILD_UPDATED、CHILD_REMOVED
+                    + new String(event.getData().getData()));
+        };
+        pathChildCache.getListenable().addListener(listener);
+        pathChildCache.start();
+        pathChildCache.start(PathChildrenCache.StartMode.NORMAL);
+    }
+
 }
