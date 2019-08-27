6f49b6b0593ac564883bd6770c4da2fe012df4c4
jobcxq <jobcxq@163.com>
2019-08-27 23:09:30 +0800

    zk

--- zookeeper/src/test/java/com/cxq/zookeeper/ZookeeperTest.java
+++ zookeeper/src/test/java/com/cxq/zookeeper/ZookeeperTest.java
@@ -22,13 +22,16 @@ public class ZookeeperTest {
 //            ZooKeeper zooKeeper = new ZooKeeper(CONNECT_STRING, 3000, null);
             //使用自定义的 Watcher 来处理事件
             ZooKeeper zooKeeper = new ZooKeeper(CONNECT_STRING, 5000, (event) -> {
-                System.out.println(event.getPath() + " : " + event.getType().name());
+                System.out.println("事件通知" + event.getPath() + " : " + event.getType().name());
             });
 
             String result = zooKeeper.create("/zookeeper/hello","'hello zookeeper !'".getBytes(),
                     ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
-            System.out.println(result);
-            Thread.sleep(50000);
+            System.out.println("result: " + result);
+
+            zooKeeper.exists("/watch",true);    //注册通知
+
+            Thread.sleep(500000);
         } catch (Exception e) {
             e.printStackTrace();
         }
